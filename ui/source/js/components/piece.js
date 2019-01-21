c7955d105de8b173db1e260811ee608b8450944c
Joseph Lawhorn <jlawhorn@degdigital.com>
2019-01-20 21:41:56 -0600

    Break tutorial stuff into modules.

---
+++ ui/source/js/components/piece.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Piece extends React.Component {
+	render() {
+		return (
+			<button className="Piece">
+			{/* TODO */}
+			</button>
+		);
+	}
+}
+
+export default Piece;
